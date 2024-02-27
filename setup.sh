af6a804942f813c9199085c768ac2ba50206ac36
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2024-02-27 20:50:59 +0100

    add setup bashfile

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Specify the Git repository URL
+GIT_REPO_URL="your_git_repo_url_here"
+
+# The directory where the Git repository will be cloned
+DOTFILES_DIR="$HOME/dotfiles"
+
+# Clone the Git repository
+if [ ! -d "$DOTFILES_DIR" ]; then
+    git clone $GIT_REPO_URL $DOTFILES_DIR
+else
+    echo "Dotfiles directory already exists. Skipping cloning."
+fi
+
+# Creating symbolic links
+ln -sf "$DOTFILES_DIR/.alacritty.toml" "$HOME/.alacritty.toml"
+ln -sf "$DOTFILES_DIR/.bash_profile" "$HOME/.bash_profile"
+ln -sf "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
+ln -sf "$DOTFILES_DIR/.condarc" "$HOME/.condarc"
+ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
+ln -sf "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
+
+echo "All dotfiles have been linked."
