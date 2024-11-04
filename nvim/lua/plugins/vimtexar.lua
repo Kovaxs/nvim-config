e2d14a3ccc17cce1953983af692677e8117cb7d7
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2024-11-04 09:37:47 +0100

    change zathura to sioyek

--- nvim/lua/plugins/vimtexar.lua
+++ nvim/lua/plugins/vimtexar.lua
@@ -14,7 +14,7 @@ return {
     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
 
     -- Set Zathura as the default PDF viewer for VimTeX
-    vim.g.vimtex_view_method = 'zathura'
+    vim.g.vimtex_view_method = 'sioyek'
     -- vim.g.vimtex_view_method = 'general'
     -- vim.g.vimtex_view_general_viewer = 'open' -- Use 'open' for macOS
     -- vim.g.vimtex_view_general_options = ''
