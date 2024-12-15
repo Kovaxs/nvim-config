f2133bf0fe9be407e6b76a5ae551de0609b035ef
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2024-12-15 12:33:56 +0100

    feat(python): better conda support

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,3 +1,16 @@
--- vim.keymap.set("n", "<F5>", ":!python %<CR>")
-vim.keymap.set('n', '<leader>gf', '<cmd>Black<CR>')
-vim.keymap.set('v', '<leader>gf', '<cmd>Black<CR>')
+local function isempty(s)
+	return s == nil or s == ""
+end
+local function use_if_defined(val, fallback)
+	return val ~= nil and val or fallback
+end
+
+-- custom python provider
+local conda_prefix = os.getenv("CONDA_PREFIX")
+if not isempty(conda_prefix) then
+	vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, conda_prefix .. "/bin/python")
+	vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, conda_prefix .. "/bin/python")
+else
+	vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, "python")
+	vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, "python3")
+end
