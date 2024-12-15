444f447555c66f0c69eca1ef902a91882d23ba08
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2024-12-15 12:43:56 +0100

    feat: better linting

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -1,28 +1,41 @@
--- General purpose linters
 return {
-  -- https://github.com/mfussenegger/nvim-lint
-  'mfussenegger/nvim-lint',
-  event = 'BufWritePost',
-  config = function ()
-    -- Define a table of linters for each filetype (not extension).
-    -- Additional linters can be found here: https://github.com/mfussenegger/nvim-lint#available-linters
-    require('lint').linters_by_ft = {
-      python = {
-        -- Uncomment whichever linters you prefer
-        'flake8',
-        'mypy',
-        'pylint',
-        'ruff'
-      }
-    }
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
 
-    -- Automatically run linters after saving.  Use "InsertLeave" for more aggressive linting.
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-      -- Only run linter for the following extensions. Remove this to always run.
-      pattern = { "*.py", },
-      callback = function()
-        require("lint").try_lint()
-      end,
-    })
-  end
+		lint.linters_by_ft = {
+			-- javascript = { "eslint_d" },
+			-- typescript = { "eslint_d" },
+			-- javascriptreact = { "eslint_d" },
+			-- typescriptreact = { "eslint_d" },
+			-- svelte = { "eslint_d" },
+			python = { "pylint", "ruff" },
+		}
+
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+
+		-- require("lint").linters.pylint.args = { "-m", "pylint", "-f", "json" }
+		require("lint").linters.pylint.cmd = "python"
+		require("lint").linters.pylint.args = {
+			"-m",
+			"pylint",
+			"-f",
+			"json",
+			"--from-stdin",
+			function()
+				return vim.api.nvim_buf_get_name(0)
+			end,
+		}
+		-- vim.keymap.set("n", "<leader>l", function()
+		--   lint.try_lint()
+		-- end, { desc = "Trigger linting for current file" })
+	end,
 }
