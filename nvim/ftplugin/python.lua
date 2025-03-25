1ec8750ba813fc1d1bba9d74157bfb1de74e6dcd
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2025-03-25 16:32:09 +0100

    style: formatting

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -6,10 +6,10 @@ local function use_if_defined(val, fallback)
 end
 
 -- custom python provider
-local conda_prefix = os.getenv("CONDA_PREFIX")
+local conda_prefix = os.getenv("CONDA_PYTHON_EXE")
 if not isempty(conda_prefix) then
-	vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, conda_prefix .. "/bin/python")
-	vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, conda_prefix .. "/bin/python")
+	vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, conda_prefix)
+	vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, conda_prefix)
 else
 	vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, "python")
 	vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, "python3")
