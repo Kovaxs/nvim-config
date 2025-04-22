0fb311da73132179035f2ac8bcfd7f09c967b237
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2025-04-22 12:34:57 +0200

    fix(luasnip): build -> run

--- nvim/lua/config/plugins/blink-cmp.lua
+++ nvim/lua/config/plugins/blink-cmp.lua
@@ -4,7 +4,7 @@ return
         'saghen/blink.cmp',
         -- optional: provides snippets for the snippet source
         dependencies = { 'rafamadriz/friendly-snippets',
-            { 'L3MON4D3/LuaSnip', version = 'v2.*', build = "make install_jsregexp" }
+            { 'L3MON4D3/LuaSnip', version = 'v2.*', run = "make install_jsregexp" }
         },
 
         -- use a release tag to download pre-built binaries
