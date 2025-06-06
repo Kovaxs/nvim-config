dad6e544f00d1773139da7eba2949de209cd74a0
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2025-06-06 22:36:00 +0200

    feat: gruvbox theme

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out,                            "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 
 -- Hey! Put lazy into the runtimepath for neovim!
@@ -25,22 +25,44 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-	spec = {
-		{
-		  "rockyzhang24/arctic.nvim",
-		  dependencies = { "rktjmp/lush.nvim" },
-		  name = "arctic",
-		  branch = "main",
-		  priority = 1000,
-		  config = function()
-		    vim.cmd("colorscheme arctic")
-		  end
-		},
-		{ import = "config.plugins" },
-	},
-	change_detection = {
-		-- automatically check for config file changes and reload the ui
-		enabled = false,
-		notify = false, -- get a notification when changes are found
-	},
+    spec = {
+        -- {
+        --     'projekt0n/github-nvim-theme',
+        --     name = 'github-theme',
+        --     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        --     priority = 1000, -- make sure to load this before all the other start plugins
+        --     config = function()
+        --         -- require('github-theme').setup({
+        --         --     -- ...
+        --         -- })
+        --
+        --         vim.cmd('colorscheme github_dark')
+        --     end,
+        -- },
+        {
+            "ellisonleao/gruvbox.nvim",
+            priority = 1000,
+            config = function()
+                vim.cmd("colorscheme gruvbox")
+                vim.o.background = "dark" -- or "light" for light mode
+            end,
+            -- opts = ...
+        },
+        -- {
+        --     "rockyzhang24/arctic.nvim",
+        --     dependencies = { "rktjmp/lush.nvim" },
+        --     name = "arctic",
+        --     branch = "main",
+        --     priority = 1000,
+        --     config = function()
+        --         vim.cmd("colorscheme arctic")
+        --     end
+        -- },
+        { import = "config.plugins" },
+    },
+    change_detection = {
+        -- automatically check for config file changes and reload the ui
+        enabled = false,
+        notify = false, -- get a notification when changes are found
+    },
 })
