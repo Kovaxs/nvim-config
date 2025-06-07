1012cc427a408d532a2dcfa0479f55bb4d1e28b1
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2025-06-07 17:36:02 +0200

    style: formatting

--- nvim/lua/config/plugins/refactoring.lua
+++ nvim/lua/config/plugins/refactoring.lua
@@ -1,12 +1,14 @@
 return {
-    "ThePrimeagen/refactoring.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-        require("refactoring").setup()
-        -- load refactoring Telescope extension
-        require("telescope").load_extension("refactoring")
-    end,
+    {
+        "ThePrimeagen/refactoring.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("refactoring").setup({})
+            -- load refactoring Telescope extension
+            require("telescope").load_extension("refactoring")
+        end,
+    }
 }
