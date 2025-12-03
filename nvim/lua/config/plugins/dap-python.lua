b47f31b8a62fb84b13e92a7ff3dfb9aa4f073b67
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2025-12-03 18:56:56 +0100

    feat: uv for dap-python

--- nvim/lua/config/plugins/dap-python.lua
+++ nvim/lua/config/plugins/dap-python.lua
@@ -1,14 +1,14 @@
 return {
-    -- https://github.com/mfussenegger/nvim-dap-python
-    'mfussenegger/nvim-dap-python',
-    ft = 'python',
-    dependencies = {
-        -- https://github.com/mfussenegger/nvim-dap
-        'mfussenegger/nvim-dap',
-    },
-    config = function()
-        -- Update the path passed to setup to point to your system or virtual env python binary
-        -- require('dap-python').setup('uv')
-        require('dap-python').setup('/Users/kovaxs/miniconda3/bin/python')
-    end
+	-- https://github.com/mfussenegger/nvim-dap-python
+	"mfussenegger/nvim-dap-python",
+	ft = "python",
+	dependencies = {
+		-- https://github.com/mfussenegger/nvim-dap
+		"mfussenegger/nvim-dap",
+	},
+	config = function()
+		-- Update the path passed to setup to point to your system or virtual env python binary
+		require("dap-python").setup("uv")
+		-- require('dap-python').setup('/Users/kovaxs/miniconda3/bin/python')
+	end,
 }
