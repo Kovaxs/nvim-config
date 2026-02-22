d8b68ddb3955633d5cc285d49d34f6c7e0ea7ac2
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2026-02-22 19:24:47 +0100

    fix: non-working dockerfile highlighting

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -1,30 +1,51 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            local configs = require("nvim-treesitter.configs")
-            configs.setup({
-                ensure_installed = { "query", "lua", "python", "latex", "markdown", "markdown_inline", "dockerfile",
-                    "bibtex", "json", "yaml", "html", "css", "markdown", "markdown_inline", "bash", "lua", "vim",
-                    "dockerfile", "gitignore", "vimdoc", "c", "nix", "regex", "cpp", "rust"
-                },
-                auto_install = true,
-                ignore_install = {},
-                modules = {},
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<CR>", -- set to `false` to disable one of the mappings
-                        node_incremental = "<CR>",
-                        scope_incremental = false,
-                        node_decremental = "<BS>",
-                    },
-                },
-            })
-        end,
-    }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = {
+					"query",
+					"lua",
+					"python",
+					"latex",
+					"markdown",
+					"markdown_inline",
+					"bibtex",
+					"json",
+					"yaml",
+					"html",
+					"css",
+					"markdown",
+					"markdown_inline",
+					"bash",
+					"lua",
+					"vim",
+					"gitignore",
+					"vimdoc",
+					"c",
+					"nix",
+					"regex",
+					"cpp",
+					"rust",
+				},
+				auto_install = false,
+				-- ignore_install = { "dockerfile", "Dockerfile" },
+				modules = {},
+				sync_install = false,
+				highlight = { enable = true, disable = { "dockerfile", "Dockerfile" } },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<CR>", -- set to `false` to disable one of the mappings
+						node_incremental = "<CR>",
+						scope_incremental = false,
+						node_decremental = "<BS>",
+					},
+				},
+			})
+		end,
+	},
 }
