8b98bb4b4cade679c0a4e94358372bace6d315ce
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2026-04-02 17:26:04 +0200

    wip: 0.12 nvim-litle

--- nvim/after/ftplugin/tex.lua
+++
@@ -1,5 +0,0 @@
-vim.keymap.set("n", "<F5>", ":VimtexCompile<CR>")
-vim.opt.wrap = true
-vim.opt.colorcolumn = ""
--- vim.opt.linebreak = true -- Wrap lines at convenient points
--- vim.opt.textwidth = 80 -- Set a preferred line length for automatic wrapping
