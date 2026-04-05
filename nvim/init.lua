d999f420efbf4dcc0213e63f7e22ffd7f1ac8ebf
Oleksandr Kovalyk <42751978+Kovaxs@users.noreply.github.com>
2026-04-05 19:45:35 +0200

    wip: clear init

--- nvim/init.lua
+++ nvim/init.lua
@@ -59,17 +59,12 @@ vim.opt.ttimeoutlen = 0 -- key code timeout
 vim.opt.autoread = true -- auto-reload changes if outside of neovim
 vim.opt.autowrite = false -- do not auto-save
 
--- vim.opt.hidden = true -- allow hidden buffers
 vim.opt.errorbells = false -- no error sounds
--- vim.opt.backspace = "indent,eol,start" -- better backspace behaviour
 vim.opt.autochdir = false -- do not autochange directories
 vim.opt.iskeyword:append("-") -- include - in words
 vim.opt.path:append("**") -- include subdirs in search
--- vim.opt.selection = "inclusive" -- include last char in selection
 vim.opt.mouse = "a" -- enable mouse support
--- vim.opt.clipboard:append("unnamedplus") -- use system clipboard
--- vim.opt.modifiable = true -- allow buffer modifications
--- vim.opt.encoding = "utf-8" -- set encoding
+vim.opt.isfname:append("@-@")
 
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
 
